35f148dab500604d61b82ca0324cf1e47136477f
Jaylin Bratcher-McEachern <651608@aacps.org>
2020-10-14 14:06:25 +0000

    I updated my zoog

--- index.java
+++ index.java
@@ -14,20 +14,21 @@ void draw() {
   rectMode(CENTER);
   // Draw Zoog's body
   stroke(0);
-  fill(150);
-  rect(mouseX, mouseY,20,100);
+  fill(255, 200, 200);
+  rect(mouseX, mouseY, 20, 100);
   // Draw Zoog's head
   stroke(0);
-  fill(255);
-  ellipse(mouseX, mouseY - 30,60,60);
+  fill(173, 216, 230);
+  ellipse(mouseX, mouseY - 30, 60, 60);
   // Draw Zoog's eyes
   fill(0);
 
   // remove the forward slashes in front of each line of code below one at a time. Remove the underline and replace with the proper code to allow your mouse to move Zoog around  💻
 
-  ellipse(mouseX - 10, mouseY - 30,16,32);
-  ellipse(mouseX + 10, mouseY - 30,16,32);
+  ellipse(mouseX - 10, mouseY - 30, 16, 32);
+  ellipse(mouseX + 10, mouseY - 30, 16, 32);
   // Draw Zoog's legs
   stroke(0);
-  line( 90, 150, 80, 160);
-  line( 110, 150, 120, 160);
+  line(mouseX - 10, mouseY + 50, mouseX - 20, mouseY + 70);
+  line(mouseX + 10, mouseY + 50, mouseX + 20, mouseY + 70);
+}
